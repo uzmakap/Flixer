c39c7861943e24c34a8b5691f02a1e74d0913fbc
uzma <uzmakap786@gmail.com>
2020-10-03 02:36:47 -0400

    flixer part 2 additions

---
+++ Flixer/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixer
+//
+//  Created by Uzma Kapadia on 10/3/20.
+//  Copyright © 2020 Uzma Kapadia. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
--- Flixer/MoviesViewController.swift
+++ Flixer/MoviesViewController.swift
@@ -65,21 +65,34 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let posterPath =  movie["poster_path"] as! String
         let posterUrl = URL(string: baseUrl + posterPath)
         
-        cell.posterView.af_setImage(withURL: posterUrl!)
+        cell.posterView.af.setImage(withURL: posterUrl!) 
+        
         
         return cell
         
     }
 
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        
+        //find the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        //pass the selected movie to the details view controller
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
+    
 
 }
